963f843123c34db54b3f5f947497d7f9a2d68e33
Andreas Chatel <59205956+999-andreas@users.noreply.github.com>
2023-03-03 11:34:47 +0100

    ajout du model pour la meteo

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ App.use(morgan('dev'));
 App.use(helmet());
 App.use(express.json());
 
-mongoose.connect(`mongodb://root:root@127.0.0.1:27017/mydatabase`, {
+mongoose.connect(`mongodb://root:root@127.0.0.1:27017/`, {
     useNewUrlParser: true
 }, (error) => {
     if(error){
--- models/motivation.js
+++ models/motivation.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 export default mongoose.model('weather', {
     date: String,
-    maxT: Float32Array,
-    minT: Float32Array,
-    RH1: Float32Array,
-    RH2: Float32Array,
-    wind:Float32Array,
-    rain:Float32Array,
-    radiation: Float32Array
+    maxT: Number,
+    minT: Number,
+    RH1: Number,
+    RH2: Number,
+    wind:Number,
+    rain:Number,
+    radiation: Number
 
 });
 
