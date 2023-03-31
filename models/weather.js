ae8ca07f7ecc33d8be615cc462c6a743ffab0e27
Andreas Chatel <andreas.chatel5@gmail.com>
2023-03-31 23:54:12 +0200

    timestamp

--- models/weather.js
+++ models/weather.js
@@ -1,9 +1,11 @@
+import { timeStamp } from "console";
 import mongoose from "mongoose";
 
 export default mongoose.model('weather', {
     date: Date,
     temp: Number,
-    hum: Number
+    hum: Number,
+    time:timeStamp
 });
 
 /*peut etre rajouter une partie meta pour mettre la date d'ajout de l'enregistrement et le user qui l'a entre*/
